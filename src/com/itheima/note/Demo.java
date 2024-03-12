ad15357a26659991a4dcc9c21a40bc6d9a11a715
xxkkyhm <1439690778@qq.com>
2024-03-12 15:47:23 +0800

    30.运算符

---
+++ src/com/itheima/note/Demo.java
@@ -0,0 +1,17 @@
+package com.itheima.note;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        boolean rs = a > b;
+        System.out.println(rs);
+
+        System.out.println(a >= b);
+        System.out.println(2 >= 2);
+        System.out.println(a < b);
+//        System.out.println(a = b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
