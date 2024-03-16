33ff12e6c0b8c8a7bae16496fe493db03f63f940
xxkkyhm <1439690778@qq.com>
2024-03-16 21:10:08 +0800

    Switch

---
+++ flow-control/src/com/itheima/branch/Switvh2.java
@@ -0,0 +1,26 @@
+package com.itheima.branch;
+
+public class Switvh2 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 0.1;
+        double b2 = 0.2;
+        double c = b + b2;
+        System.out.println(c);
+        switch (a){
+
+        }
+
+        int i = 20;
+        int d = 20;
+        switch (i){
+            case 10:
+
+            break;
+            case 20:
+            break;
+        }
+
+
+    }
+}
