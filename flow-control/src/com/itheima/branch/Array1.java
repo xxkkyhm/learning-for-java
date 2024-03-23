469188ad52e0ec2236e97f56d8ec2cc83c27fa17
xxkkyhm <1439690778@qq.com>
2024-03-23 21:19:00 +0800

    Array1

---
+++ flow-control/src/com/itheima/branch/Array1.java
@@ -0,0 +1,19 @@
+package com.itheima.branch;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+        int[] ages = new int[]{12,24,36};
+        double[] scores = new double[]{89.9,99.5,59.5,88};
+        System.out.println(ages);
+        System.out.println(scores);
+
+//                           简化
+        int[] ages2 = {12,24,36};
+        double[] scores2 = {89.9,99.5,59.5,88};
+
+
+        int ages3[] = {12,24,36};
+        double scores3[] = {89.9,99.5,59.5,88};
+    }
+}
---
+++ flow-control/src/com/itheima/branch/Array2.java
@@ -0,0 +1,11 @@
+package com.itheima.branch;
+
+public class Array2 {
+    public static void main(String[] args) {
+        int[] arr = {12,24,36};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
