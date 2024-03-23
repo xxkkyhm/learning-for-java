bdce3cd7c768bd71cac63ee6313fd4eedc067d27
xxkkyhm <1439690778@qq.com>
2024-03-23 21:34:21 +0800

    Array3

---
+++ flow-control/src/com/itheima/branch/Array3.java
@@ -0,0 +1,19 @@
+package com.itheima.branch;
+
+public class Array3 {
+    public static void main(String[] args) {
+        int[] ages = {12,24,36};
+
+//        System.out.println(ages[0]);
+//        System.out.println(ages[1]);
+//        System.out.println(ages[2]);
+
+        for (int i =0; i < ages.length; i++){
+            System.out.println(ages[i]);
+        }
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }
+    }
+}
