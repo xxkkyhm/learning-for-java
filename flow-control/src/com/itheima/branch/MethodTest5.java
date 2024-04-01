1b947279659369b58ffab8264a64c8957679598c
xxkkyhm <1439690778@qq.com>
2024-04-01 17:44:04 +0800

    MethodTest5

--- flow-control/src/com/itheima/branch/MethodTest5.java
+++ flow-control/src/com/itheima/branch/MethodTest5.java
@@ -5,11 +5,23 @@ public class MethodTest5 {
         int a = 10;
         change(a);
         System.out.println(a);
+
+        System.out.println("==============");
+
+        int[] arrs = new int[]{10,20,30};
+        change2(arrs);
+        System.out.println(arrs[1]);
     }
 
     public static void change(int a){
         System.out.println(a);
         a = 20;
         System.out.println(a);
+    }
+
+    public static void change2(int[] arrs){
+        System.out.println(arrs[1]);
+        arrs[1] = 222;
+        System.out.println(arrs[1]);
     }
 }
