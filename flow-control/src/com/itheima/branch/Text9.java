5a681b09283a53c79ac9f51a3469838bc6f0d817
xxkkyhm <1439690778@qq.com>
2024-04-27 20:22:26 +0800

    Text9

--- flow-control/src/com/itheima/branch/Text9.java
+++ flow-control/src/com/itheima/branch/Text9.java
@@ -1,12 +1,20 @@
 package com.itheima.branch;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Text9 {
     public static void main(String[] args) {
 
         int[] userNumbers = userSelectNumber();
+        System.out.println("投注的号码");
         printArray(userNumbers);
+
+        int[] luckNmumbers = creatLuckNumbers();
+        System.out.println("中奖的号码");
+        printArray(luckNmumbers);
+
+        judge(userNumbers,luckNmumbers);
     }
 
     public static void printArray(int[] arr){
@@ -61,5 +69,59 @@ public class Text9 {
             }
         }
         return false;
+    }
+
+    public static int[] creatLuckNumbers(){
+        int[] numbers = new int[7];
+
+        Random r = new Random();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+
+            while (true){
+                int number = r.nextInt(33) + 1;
+
+                if (!exist(numbers,number)){
+                    numbers[i] = number;
+                    break;
+                }
+            }
+        }
+        numbers[6] = r.nextInt(16) + 1;
+        return numbers;
+    }
+
+    private static void judge(int[] userNumbers,int[] luckNumbers){
+        int redCount = 0;
+        int blueCount = 0;
+
+        for (int i = 0; i < userNumbers.length; i++) {
+            for (int j = 0; j < luckNumbers.length; j++) {
+                if (userNumbers[i] == luckNumbers[j]){
+                    redCount++;
+                    break;
+                }
+            }
+        }
+        blueCount = userNumbers[6] == luckNumbers[6] ? 1 : 0;
+
+        System.out.println("您命中的红球数量：" + redCount);
+        System.out.println("您命中的蓝球数量：" + blueCount);
+
+        if (redCount == 6 && blueCount == 1){
+            System.out.println("中奖1000万");
+        }else if (redCount == 6 && blueCount == 1){
+            System.out.println("中奖500万");
+        }else if (redCount == 5 && blueCount == 1){
+            System.out.println("中奖3000");
+        }else if (redCount == 5 && blueCount == 0 || redCount == 4 && blueCount == 1){
+            System.out.println("中奖200");
+        }else if (redCount == 4 && blueCount == 0 || redCount == 3 && blueCount == 1){
+            System.out.println("中奖10");
+        }else if (redCount < 3 && blueCount == 1){
+            System.out.println("中奖5");
+        }else {
+            System.out.println("无中奖");
+        }
     }
 }
